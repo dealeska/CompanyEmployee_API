988820d82fabe419b354b8f93d012067018d56e9
Alesya Deschenya <alesya.deschenya@gmail.com>
2022-12-26 13:32:03 +0300

    Service layer

--- InnoGotchi_Server/Extensions/ServiceExtensions.cs
+++ InnoGotchi_Server/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
 using Repository;
+using Service.Contracts;
+using Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace InnoGotchi_Server.Extentions
 {
@@ -27,5 +30,12 @@ namespace InnoGotchi_Server.Extentions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
     }
 }
