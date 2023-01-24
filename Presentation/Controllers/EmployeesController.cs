83c8fe666f4f811eb2697414285c58232c37b329
Alesya Deschenya <alesya.deschenya@gmail.com>
2023-01-24 11:08:13 +0300

    Add validation

--- Presentation/Controllers/EmployeesController.cs
+++ Presentation/Controllers/EmployeesController.cs
@@ -34,6 +34,10 @@ namespace Presentation.Controllers
         {
             if (employee is null)
                 return BadRequest("EmployeeForCreationDto object is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
             var employeeToReturn = _service.EmployeeService.CreateEmployeeForCompany(companyId, employee, trackChanges: false);
             return CreatedAtRoute("GetEmployeeForCompany", new
             {
@@ -56,6 +60,9 @@ namespace Presentation.Controllers
             if (employee is null)            
                 return BadRequest("EmployeeForUpdateDto object is null");
 
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
             _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee, compTrackChanges: false, empTrackChanges: true);
             return NoContent();
 
@@ -68,7 +75,12 @@ namespace Presentation.Controllers
                 return BadRequest("patchDoc object sent from client is null.");
 
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
-            patchDoc.ApplyTo(result.employeeToPatch);
+            patchDoc.ApplyTo(result.employeeToPatch, ModelState);
+
+            TryValidateModel(result.employeeToPatch);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
 
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
             return NoContent();
